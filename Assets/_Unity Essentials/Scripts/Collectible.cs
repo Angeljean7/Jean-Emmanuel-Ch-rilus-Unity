f2dd34a92c34620655358147a6739b06a3820eb7
Angeljean7 <xwars777@gmail.com>
2024-09-28 13:35:22 -0400

    Envois

---
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    public GameObject onCollectEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(0, rotationSpeed, 0);
+
+    }
+    
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+
+     if(other.CompareTag("Player")) {
+    // Destroy the collectible
+       Destroy(gameObject);
+
+       // Instantiate the particle effect
+       Instantiate(onCollectEffect, transform.position, transform.rotation);    
+     }
+
+       
+
+    }
+
+
+}
